c37f09078354774964cf925aab5931682d912d35
Nikola Panayotov <10470335+NikolaPanayotov@users.noreply.github.com>
2021-04-12 22:38:20 -0700

    Able to communicate to redis container by running fake_chat_watcher/main.py on local machine!

--- fake_chat_watcher/main.py
+++ fake_chat_watcher/main.py
@@ -2,18 +2,29 @@
 Fake app meant to replicate a chat watcher locally
 This is meant to run in conjuction w/ a redis
 """
-
+# Builtins
 from random import randrange
 import time
+# Packages
+import redis
+# Local imports
 
 TWITCH_MESSAGES = [
     "PogChamp",
-    "Kappa Kappa KAPPA",
-    "INJOKER 420"
+    "Kappa",
+    "Keepo"
 ]
 
 if __name__ == "__main__":
+    hostname = '0.0.0.0'
+    print(f"Attempting to connect to redis at: {hostname}")
+    rdb = redis.Redis(host=hostname, port=6379, db=0)
     while True:
         # TODO: Send these to Redis instead smiley face
-        print(TWITCH_MESSAGES[randrange(len(TWITCH_MESSAGES))])
+        message = TWITCH_MESSAGES[randrange(len(TWITCH_MESSAGES))]
+        print(f"{message}")
+        try:
+            rdb.incr(message, 1)
+        except redis.exceptions.ConnectionError:
+            print("ERROR CONNECTING TO REDIS!")
         time.sleep(randrange(10, 20)/10)
