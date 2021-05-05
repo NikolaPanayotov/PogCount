fed31a3a05ea5e8088f3d57cd38dc619771b9870
Nikola Panayotov <10470335+NikolaPanayotov@users.noreply.github.com>
2021-05-04 23:09:26 -0700

    Adding bot manager functionality

---
+++ bot_manager/bot_manager.py
@@ -0,0 +1,59 @@
+# Packages
+import docker
+from dotenv import load_dotenv
+import time
+# Local imports
+from twitch_errors import OAuthError, TwitchAPIError
+from twitch_api import get_twitch_oauth, get_top_channels
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    client = docker.from_env()
+    auth_token = get_twitch_oauth()
+    bots = []
+    bot_command = "python -u bot.py "
+    docker_network = "twitch-emote-tracker_default"
+    while True:
+        try:
+            top20 = None
+            try:
+                top20 = get_top_channels(auth_token, 100)
+            except OAuthError:
+                print("Refreshing oauth token!")
+                auth_token = get_twitch_oauth()
+            except TwitchAPIError:
+                print("New error!")
+            if top20 is not None:
+                bots = [container for container in client.containers.list()
+                        if "chat-watcher" in str(container.image)]
+                print(f"Current bots running: {bots}")
+                for container in bots:
+                    if container.name not in top20:
+                        print("{container.name} is not in the top 20!"
+                              "Removing chat watcher...")
+                        container.kill()
+                    else:
+                        print("Already watching {container.name}!")
+                        top20.remove(container.name)
+                for channel in top20:
+                    print(f"Watching {channel}...")
+                    container_cmd = bot_command + channel
+                    client.containers.run('chat-watcher:latest',
+                                          name=channel,
+                                          network=docker_network,
+                                          detach=True,
+                                          command=container_cmd)
+                    print(f"{channel} container created!")
+                print("Time to sleep!")
+                time.sleep(15*60)
+        except KeyboardInterrupt:
+            print("Cleaning up!")
+            bots = [container for container in client.containers.list()
+                    if "chat-watcher" in str(container.image)]
+            for container in bots:
+                print(f"Stopping {container.name}")
+                container.kill()
+                print(f"Removing {container.name}")
+                container.remove()
+            break
---
+++ bot_manager/twitch_api.py
@@ -0,0 +1,50 @@
+# Packages
+import os
+import requests
+import json
+
+
+def get_twitch_oauth():
+    oauth_url = "https://id.twitch.tv/oauth2/token"
+    client_id = os.getenv('CLIENT_ID')
+    client_secret = os.getenv('CLIENT_SECRET')
+    query = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "grant_type": "client_credentials"
+    }
+    response = requests.post(oauth_url, params=query)
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        token = result['access_token']
+        return token
+    else:
+        print(f"Error getting oauth token: {response.text}")
+        raise TwitchAPIError
+
+
+def get_top_channels(oauth, quantity):
+    streams_url = "https://api.twitch.tv/helix/streams"
+    client_id = os.getenv('CLIENT_ID')
+    headers = {
+        'Authorization': "Bearer " + oauth,
+        "client-id": client_id,
+    }
+    query = {
+        "first": quantity,
+    }
+    response = requests.get(streams_url, params=query, headers=headers)
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        channels = []
+        for channel in result['data']:
+            channels.append(channel['user_login'])
+        return channels
+    elif response.status_code == 401:
+        print("Oauth invalid!")
+        print(response.text)
+        raise OAuthError
+    else:
+        print(response.status_code)
+        print(response.text)
+        raise TwitchAPIError
